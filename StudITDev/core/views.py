e5a7059878bd5ecd67dd035b3de10d89c3e51d44
felixity1917 <77635620+felixity1917@users.noreply.github.com>
2024-09-20 14:49:04 +0530

    Add: Testing with Todos

--- StudITDev/core/views.py
+++ StudITDev/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
     # return HttpResponse("Test...")
     return render(request, "home.html")
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": items})
